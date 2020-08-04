80b817d057c8b53a64670e30b65bc6d6a41efea0
1nf0rmagician <msandermann@gmail.com>
2020-08-04 10:55:12 +0200

    Add FoodCompartement resource

---
+++ src/MyApplication.Resources.MyResource/FoodCompartement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Device.Location;
+using System.Runtime.Serialization;
+using Moryx.AbstractionLayer;
+using Moryx.AbstractionLayer.Products;
+using Moryx.AbstractionLayer.Resources;
+using MyApplication.Capabilities;
+
+namespace MyApplication.Resources.MyResource
+{
+    [ResourceRegistration] // Only necessary for dependency injection like logging or parallel operations
+    public class FoodCompartement : PublicResource, IFoodCompartement
+    {
+        [DataMember, EditorBrowsable]
+        [Description("Location of the Food Compartement")]
+        public GeoCoordinate Location { get; set; }
+
+        [DataMember, EditorBrowsable]
+        [Description("Maximum capacity of the food compartement")]
+        public int Capacity { get; set; }
+
+        [DataMember]
+        public ICollection<ProductInstance> CurrentFoodSupply { get; set; }
+
+        public event EventHandler FoodSupplyChanged;
+    }
+}
---
+++ src/MyApplication/Resources/IFoodCompartement.cs
@@ -0,0 +1,31 @@
+﻿using Moryx.AbstractionLayer.Products;
+using Moryx.AbstractionLayer.Resources;
+using System;
+using System.Collections.Generic;
+using System.Device.Location;
+
+namespace MyApplication.Resources
+{
+    public interface IFoodCompartement : IPublicResource
+    {
+        /// <summary>
+        /// Location of this food compartement
+        /// </summary>
+        GeoCoordinate Location { get; }
+
+        /// <summary>
+        /// Total capacity of this food compartement in liter
+        /// </summary>
+        int Capacity { get; }
+
+        /// <summary>
+        /// Current food supply in the compartement
+        /// </summary>
+        ICollection<ProductInstance> CurrentFoodSupply { get; set; }
+
+        /// <summary>
+        /// Event raise, when availability of items in the compartement changed
+        /// </summary>
+        event EventHandler FoodSupplyChanged;
+    }
+}
